8bacb52ee33fcb7c0f2ae7def5ebf5a563286201
JayBW1 <jacobbartolywilliams@gmail.com>
2024-01-17 20:09:50 +0000

    Added Functionality

--- Starship_Trace.py
+++ Starship_Trace.py
@@ -4,40 +4,39 @@ main_menu_list = ["End Session","View Logs","Record Log","Edit Log",\
 log_location_list = ["London","Birmingham","Manchester","Southampton",\
 "Edinburgh","Belfast","Newcastle"]
 def location_listing():
-    print("Location List:")
+    print("\nLocation List:")
     for location in log_location_list:
         print(f"-\t{location}")
         
 log_goods_type_list = ["Electronics","Clothing","Sporting","Toys",\
 "Mechanicals","Crafts","Houshold Goods","Physical Medias","Foods","Other"]
 def goods_type_listing():
-    print("Goods Type List:")
+    print("\nGoods Type List:")
     for goods in log_goods_type_list:
         print(f"-\t{goods}")
 
 def main_menu_list_output():
-    print("\nMain Menu:") # output menu
+    print("Main Menu:") # output menu
     for i, option in enumerate(main_menu_list):print(f"[{i}] {option}")
 
 def view_logs():
     global log_list
     log_list = open("log_list.txt" , "+a")
-    log_list.seek(0)
-    log_list_read = log_list.read()
-    if log_list_read == "":
-        print("No Logs Available")
+    log_list.seek(0);log_list_read = log_list.read()
+    if log_list_read == "":print("No Logs Available\n")
     else:
-        for i, log in enumerate(log_list_read,1):print(f"[{i}] {log}")
+        for log in enumerate(log_list_read,1):print(f"\n{log_list_read}");break
     log_list.close() # output list
 
 def record_log():
     with open("log_list.txt" , "+a") as log_list:
+        global log_count
         current_entry = "log_number"
         while True:
             if current_entry == "log_number":
-                log_number = input("\nRecord Menu\n[0] Main Menu\n\
+                log_number = input("\nRecord Menu:\n[0] Main Menu\n\
 Enter Log Number (6 Digits): ") # input check
-                if log_number == "0":print("Back To Main Menu");break
+                if log_number == "0":print("Back To Main Menu\n");break
                 elif log_number.isnumeric():
                     if log_number in log_list:print("Log Already Recorded")
                     elif len(log_number) != 6:print("Invalid Digit Count")
@@ -47,7 +46,7 @@ Enter Log Number (6 Digits): ") # input check
             elif current_entry == "log_name":
                 log_name = input("\n[0] Main Menu\n[-1] Back\n\
 Enter Log Name (Character Limit [20]): ")
-                if log_name == "0":print("Back To Main Menu");break
+                if log_name == "0":print("Back To Main Menu\n");break
                 elif log_name == "-1":
                     print("Back To Previous Entry")
                     current_entry = "log_number"
@@ -61,7 +60,7 @@ Enter Log Name (Character Limit [20]): ")
             elif current_entry == "log_tracking":
                 log_tracking = input("\n[0] Main Menu\n[-1] Back\n\
 Enter Log Tracking Number (8 Digits): ")
-                if log_tracking == "0":print("Back To Main Menu");break
+                if log_tracking == "0":print("Back To Main Menu\n");break
                 elif log_tracking == "-1":
                     print("Back To Previous Entry")
                     current_entry = "log_name"
@@ -77,7 +76,7 @@ Enter Log Tracking Number (8 Digits): ")
             elif current_entry == "log_location":
                 log_location = input("\n[0] Main Menu\n[-1] Back\n\
 [1] Location List\nEnter Log Location: ")
-                if log_location == "0":print("Back To Main Menu");break
+                if log_location == "0":print("Back To Main Menu\n");break
                 elif log_location == "-1":
                     print("Back To Previous Entry")
                     current_entry = "log_tracking"
@@ -89,20 +88,21 @@ Enter Log Tracking Number (8 Digits): ")
             elif current_entry == "log_goods_type":
                 log_goods_type = input("\n[0] Main Menu\n[-1] Back\n\
 [1] Goods Type List\nEnter Log Goods Type: ")
-                if log_goods_type == "0":print("Back To Main Menu");break
+                if log_goods_type == "0":print("Back To Main Menu\n");break
                 elif log_goods_type == "-1":
                     print("Back To Previous Entry")
                     current_entry = "log_location"
                 elif log_goods_type == "1":goods_type_listing()
                 elif log_goods_type in log_goods_type_list:
-                    print("Valid Goods Type")
+                    print("Valid Goods Type\n")
                     current_entry = "full_log"
                 else:print("Invalid Input")
                 
             elif current_entry == "full_log":
-                full_log = f"\nLog Number: {log_number}\nLog Name: {log_name}\n\
-Tracking: {log_tracking}\nLocation: {log_location}\nGoods: {log_goods_type}"
-                print(f"{full_log}\nLog Recorded")
+                full_log = f"Log Number:\t{log_number}\nLog Name:\t\
+{log_name}\nTracking:\t{log_tracking}\nLocation:\t{log_location}\nGoods:\t\t\
+{log_goods_type}\n\n"
+                print(f"Log Recorded:\n{full_log}")
                 log_list.write(full_log)
                 log_list.close();break
 
@@ -112,7 +112,7 @@ def edit_log():
     log_list_read = log_list.read()
     while True:
         if log_list_read == "":
-            print("No Logs Available");break
+            print("No Logs Available\n");break
         else:
             for i, log in enumerate(log_list_read,1):print(f"[{i}] {log}")
             delete_option = input("\nEdit Menu:\n[0] Main Menu\n\
@@ -126,7 +126,7 @@ def delete_log():
     log_list_read = log_list.read()
     while True:
         if log_list_read == "":
-            print("No Logs Available");break
+            print("No Logs Available\n");break
         else:
             for i, log in enumerate(log_list_read,1):print(f"[{i}] {log}")
             delete_option = input("\nDelete Menu\n[0] Main Menu\n\
@@ -135,7 +135,7 @@ Enter Log Index: ")
     log_list.close() # output list
 
 while True: # main loop
-    main_menu_list_output();menu_option = input("\nEnter Option: ")
+    main_menu_list_output();menu_option = input("Enter Option: ")
     
     if menu_option == "0": # end session
         print("Session Ended");break
@@ -148,4 +148,4 @@ while True: # main loop
     
     elif menu_option == "4":delete_log() # delete log
         
-    else:print("Invalid Input") # input check
+    else:print("Invalid Input\n") # input check
